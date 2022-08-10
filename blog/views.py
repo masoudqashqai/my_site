5f28b55aaf82d211b1349b1b85c62ddb1c9bf681
masoudQashqaei <m.ghashghaei.1993@gmail.com>
2022-08-10 14:42:37 +0430

    post-detail

--- blog/views.py
+++ blog/views.py
@@ -68,14 +68,22 @@ def get_date(post):
     return post['date']
 
 def starting_page(request):
-    sorted_posts = sorted(all_posts,key=get_date)
+    sorted_posts = sorted(all_posts, key=get_date)
     latest_posts = sorted_posts[-3:]
-    return render(request,"blog/index.html",{
-        "posts":latest_posts
+    return render(request, "blog/index.html", {
+        "posts": latest_posts
     })
 
+
 def posts(request):
-    return render(request, "blog/all-posts.html")
+    return render(request, "blog/all-posts.html", {
+        "all_posts": all_posts
+    })
+
 
 def post_detail(request, slug):
-    return render(request, 'blog/post-detail.html')
+
+    identified_post = next(post for post in all_posts if post['slug'] == slug)
+    return render(request, 'blog/post-detail.html', {
+        'post': identified_post
+    })
